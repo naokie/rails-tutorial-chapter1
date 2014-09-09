19c7a86de54be7996be810205f20ab8be8569ebc
Naoki Endoh <naoki.endoh@gmail.com>
2014-09-09 18:21:51 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails1stApp::Application.config.secret_key_base = '90534e5763662fbfa7d09acdcab4e56d67d79e1375fec02e51398016a1bc9d28025a7b60147ddda7faa7cf8716f52e7a7bfb6516ad83ff892c15e9eba1ed833e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails1stApp::Application.config.session_store :cookie_store, key: '_rails_1st_app_session'
